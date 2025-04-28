5fbcff43544d9d2c22e76740b2906ec4b310c5bd
Leonardo <novaesleo2005@gmail.com>
2025-04-28 15:15:49 -0300

    Criando a janela de descanso

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Sec2 from './components/Sec2'
 import Sec3 from './components/Sec3'
 import Sec5 from './components/Sec5'
 import Sec4 from './components/Sec4'
+import './styles/Main.css'
 
 function App() {
   return (
-    <>
+    <div className='main'>
       <Header/>
       <Sec1 />
       <Sec2/>
       <Sec3/>
       <Sec4/>
       <Sec5/>
-    </>
+    </div>
   )
 }
 
--- src/components/Sec3.jsx
+++ src/components/Sec3.jsx
@@ -2,7 +2,7 @@ import '../styles/Sec3.css'
 
 export default function Sec3() {
   return (
-    <section>
+    <section className='sec3'>
     </section>
   )
 }
